5c09670d7bced5eddac99c229052a3627cb84fe5
Tonio Adams <avcx12@gmail.com>
2023-06-05 20:27:55 -0600

    Implemented photo component

---
+++ src/components/Photo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Photo = ({src, alt}) => {
+
+    return (
+      <li>
+        <img
+          src={src}
+          alt={alt}
+        />
+      </li>
+    );
+
+}
+
+export default Photo
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,32 +1,32 @@
 import React from "react";
+import Photo from "./Photo";
+import NoResults from "./NoResults";
+
+let i = 0;
 
 const PhotoContainer = ({data}) => {
 
-    console.log(data);
-
+    let resultArray = data.photo || [];
+    console.log(resultArray, i);
+    i++
+    let photos;
+    if (resultArray.length > 0) {
+        photos = resultArray.map((photo) => (
+          <Photo
+            key={photo.id}
+            src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_q.jpg`}
+            alt={photo.title}
+          />
+        ));
+    } else {
+        photos = <NoResults />
+    }
     return (
-        <div className="photo-container">
+      <div className="photo-container">
         <h2>Results</h2>
-        <ul>
-          <li>
-            <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
-          </li>
-          <li>
-            <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />
-          </li>
-          <li>
-            <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
-          </li>
-          <li>
-            <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-          </li>
-          {/* <li className="not-found">
-            <h3>No Results Found</h3>
-            <p>You search did not return any results. Please try again.</p>
-          </li> */}
-        </ul>
+        <ul>{photos}</ul>
       </div>
-    )
+    );
 }
 
-export default PhotoContainer
+export default PhotoContainer;
